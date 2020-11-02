7622f4d5f600da96e0e01a99aac98e625555f244
salvador lihue yagusz <salvadoryagusz@gmail.com>
2020-11-02 15:39:40 -0300

    Nada

--- ClusteringHumano/src/view/MainView.java
+++ ClusteringHumano/src/view/MainView.java
@@ -1,20 +1,9 @@
 package view;
 
-import java.awt.Color;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableModel;
 
 //import View.VentanaSecundaria;
 
