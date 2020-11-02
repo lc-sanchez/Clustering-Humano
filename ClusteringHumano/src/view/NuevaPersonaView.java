acd61dbb0e3164cf5332fea53695d56c53b6e888
Usuario <Usuario@DESKTOP-RJ2DC5V>
2020-11-02 16:37:22 -0300

    Se agrega nuevas clases.

--- ClusteringHumano/src/view/NuevaPersonaView.java
+++ ClusteringHumano/src/view/NuevaPersonaView.java
@@ -1,23 +1,19 @@
 package view;
 
-
 import javax.swing.JFrame;
 
-
-public class VerPersonasView extends JFrame {
-
+public class NuevaPersonaView extends JFrame{
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public VerPersonasView() {
-		//Se establecen las caracteristicas de la ventana verPersonas
+
+	public NuevaPersonaView() {
+		//Se establecen las caracteristicas de la ventana agregarPersona
 		this.setBounds(600, 50, 500, 600);
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		this.getContentPane().setLayout(null);
-		
-		
 	}
 
 }
