30f24064bf9e4484d1695d18d9f0099ca0bfa642
Usuario <Usuario@DESKTOP-RJ2DC5V>
2020-11-02 14:16:28 -0300

    Mas cambios en el view

--- ClusteringHumano/src/view/MainView.java
+++ ClusteringHumano/src/view/MainView.java
@@ -16,6 +16,30 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 
+//import View.VentanaSecundaria;
+
 public class MainView {
+	private JFrame frame;
+	
+	public MainView() {
+		inicializar();
+	}
+	
+	public void inicializar() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Abrir Ventana secundaria");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//VentanaSecundaria ventana = new VentanaSecundaria();
+				//ventana.frame.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(213, 193, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
 
 }
