7c31021366764192336472ae3b7f7698f0a357ee
salvador lihue yagusz <salvadoryagusz@gmail.com>
2020-11-02 16:37:39 -0300

    cambio minimo en persona

--- ClusteringHumano/src/models/Persona.java
+++ ClusteringHumano/src/models/Persona.java
@@ -1,5 +1,26 @@
 package models;
 
-public class Persona {
+import java.util.ArrayList;
 
+public class Persona {
+	private ArrayList<Integer> intereses;
+	private String nombre;
+	
+	public Persona(String nombre, int deportes, int musica, int espectaculo, int ciencia){
+		setNombre(nombre);
+		
+		intereses = new ArrayList<Integer>();
+		intereses.add(deportes);
+		intereses.add(musica);
+		intereses.add(espectaculo);
+		intereses.add(ciencia);
+	}
+
+	
+	
+	
+	
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
 }
